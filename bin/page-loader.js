a0db6f6f9935f03154e32d7526b3edf9223fb292
georg3103 <gnmkrtchyan@gmail.com>
2020-08-10 18:44:08 +0300

    feat(bin)

---
+++ bin/page-loader.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import commander from 'commander';
+import { version } from '../package.json';
+
+import downloadPage from '..';
+
+commander
+  .version(version)
+  .description('The utility for downloading page from the web')
+  .arguments('<url> <output>')
+  .action((url, output) => {
+    downloadPage(url, output)
+      .then(() => {
+        console.log('page is loaded');
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.error('Download completed with error:', err);
+        process.exit(1);
+      });
+  })
+  .parse(process.argv);
